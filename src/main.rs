2be53b362bbb90b3aa588c5d0e9735f822df8bd0
Kirill Nikitin <locke23rus@gmail.com>
2022-12-05 00:10:31 +0200

    Add day 4 in rust

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
 // mod year2019;
 // mod year2020;
-mod year2021;
+// mod year2021;
+mod year2022;
 
 fn main() {
-    // year2021::day01::solve();
-    // year2021::day02::solve();
-    // year2021::day03::solve();
-    // year2021::day04::solve();
-    // year2021::day05::solve();
-    // year2021::day06::solve();
-    // year2021::day07::solve();
-    // year2021::day08::solve();
-    // year2021::day09::solve();
-    // year2021::day10::solve();
-    // year2021::day11::solve();
-    year2021::day12::solve();
+	// year2021::day01::solve();
+	// year2021::day02::solve();
+	// year2021::day03::solve();
+	// year2021::day04::solve();
+	// year2021::day05::solve();
+	// year2021::day06::solve();
+	// year2021::day07::solve();
+	// year2021::day08::solve();
+	// year2021::day09::solve();
+	// year2021::day10::solve();
+	// year2021::day11::solve();
+	// year2021::day12::solve();
+	year2022::day04::solve();
 }
