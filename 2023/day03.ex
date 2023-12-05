37e64798cd6cc4430fa90329681a540e9f7dc8e6
Kirill Nikitin <locke23rus@gmail.com>
2023-12-06 01:18:25 +0200

    Update part 1

--- 2023/day03.ex
+++ 2023/day03.ex
@@ -9,21 +9,21 @@ defmodule Day03 do
       number_slices = Regex.scan(~r/\d+/, line, return: :index) |> List.flatten()
 
       number_slices
-      |> Enum.map(fn {start, len} ->
-        area_x = if start > 0, do: start - 1, else: 0
-        area_length = if start > 0, do: len + 2, else: len + 1
-        area_y = if y > 0, do: y - 1, else: 0
+      |> Enum.map(fn {x, len} ->
+        area_x = max(0, x - 1)
+        area_y = max(0, y - 1)
+        area_length = if x > 0, do: len + 2, else: len + 1
         area_height = if y == 0 || y == max_y, do: 2, else: 3
 
-        adjusted? =
+        adjusted_to_symbol? =
           Enum.slice(lines, area_y, area_height)
           |> Enum.map(&String.slice(&1, area_x, area_length))
           |> Enum.join("")
           |> String.match?(~r/[^.0-9]/)
 
-        str = String.slice(line, start, len)
+        str = String.slice(line, x, len)
 
-        if adjusted?, do: String.to_integer(str), else: 0
+        if adjusted_to_symbol?, do: String.to_integer(str), else: 0
       end)
     end)
     |> List.flatten()
@@ -33,11 +33,11 @@ defmodule Day03 do
   def part2(input) do
     input
     |> String.split("\n", trim: true)
-    |> String.length()
+    |> length()
   end
 end
 
 input = File.read!("./input/03.txt")
 
 IO.puts(Day03.part1(input))
-# IO.puts(Day02.part2(input))
+IO.puts(Day03.part2(input))
