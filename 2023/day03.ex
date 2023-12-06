34e1cba85fd2ebb516d530aba2069c74893c549d
Kirill Nikitin <locke23rus@gmail.com>
2023-12-06 20:36:43 +0200

    day 3 part 2

--- 2023/day03.ex
+++ 2023/day03.ex
@@ -31,9 +31,51 @@ defmodule Day03 do
   end
 
   def part2(input) do
-    input
-    |> String.split("\n", trim: true)
-    |> length()
+    lines = String.split(input, "\n", trim: true)
+    max_y = length(lines)
+
+    lines
+    |> Enum.with_index()
+    |> Enum.map(fn {line, y} ->
+      gears = Regex.scan(~r/[*]/, line, return: :index) |> List.flatten()
+
+      gears
+      |> Enum.map(fn {x, _len} ->
+        area_y = max(0, y - 1)
+        area_height = if y == 0 || y == max_y, do: 2, else: 3
+
+        surrounding_lines = Enum.slice(lines, area_y, area_height)
+
+        part_numbers =
+          surrounding_lines
+          |> Enum.map(fn surrounding_line ->
+            number_slices =
+              Regex.scan(~r/\d+/, surrounding_line, return: :index) |> List.flatten()
+
+            number_slices
+            |> Enum.filter(fn {slice_start, len} ->
+              slice_end = slice_start + len
+
+              (x >= slice_start && x < slice_end) ||
+                (x - 1 >= slice_start && x - 1 < slice_end) ||
+                (x + 1 >= slice_start && x + 1 < slice_end)
+            end)
+            |> Enum.map(fn {start, len} ->
+              surrounding_line |> String.slice(start, len) |> String.to_integer()
+            end)
+          end)
+          |> List.flatten()
+
+        if length(part_numbers) == 2 do
+          [a, b] = part_numbers
+          a * b
+        else
+          0
+        end
+      end)
+    end)
+    |> List.flatten()
+    |> Enum.sum()
   end
 end
 
