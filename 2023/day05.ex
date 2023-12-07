fbe38e5a048f6bbd6b2ff067027cd778aa3fe7a1
Kirill Nikitin <locke23rus@gmail.com>
2023-12-08 00:02:44 +0200

    solve part 1

--- 2023/day05.ex
+++ 2023/day05.ex
@@ -4,29 +4,33 @@ defmodule Day05 do
     [_, seeds] = String.split(seeds, ": ")
     seeds = seeds |> String.split(" ") |> Enum.map(&String.to_integer/1)
 
-    list_of_maps =
+    list_of_ranges =
       maps
       |> Enum.map(fn map ->
         [_, map] = String.split(map, ":\n")
 
-        ranges =
-          map
-          |> String.split("\n", trim: true)
-          |> Enum.map(fn line ->
-            [dst, src, len] =
-              line |> String.split(" ", trim: true) |> Enum.map(&String.to_integer/1)
+        map
+        |> String.split("\n", trim: true)
+        |> Enum.map(fn line ->
+          [dst, src, len] =
+            line |> String.split(" ", trim: true) |> Enum.map(&String.to_integer/1)
 
-            {dst, src, len}
-          end)
-
-        ranges
-        |> Enum.reduce(%{}, fn {dst, src, len}, acc ->
-          0..(len - 1) |> Enum.reduce(acc, fn i, acc -> Map.put(acc, src + i, dst + i) end)
+          {dst, src, len}
         end)
       end)
 
-    list_of_maps
-    |> Enum.reduce(seeds, fn map, acc -> acc |> Enum.map(&Map.get(map, &1, &1)) end)
+    list_of_ranges
+    |> Enum.reduce(seeds, fn ranges, acc ->
+      acc
+      |> Enum.map(fn seed ->
+        ranges
+        |> Enum.find_value(fn {dst, src, len} ->
+          if seed >= src && seed <= src + len - 1 do
+            dst + (seed - src)
+          end
+        end) || seed
+      end)
+    end)
     |> Enum.min()
   end
 
