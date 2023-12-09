e7c17da74c674571b6c92c1da6e01cfe05315dec
Kirill Nikitin <locke23rus@gmail.com>
2023-12-09 02:50:07 +0200

    day 6 part 1

---
+++ 2023/day06.ex
@@ -0,0 +1,29 @@
+defmodule Day06 do
+  def part1(input) do
+    [times, distances] =
+      input
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn line ->
+        Regex.scan(~r/\d+/, line)
+        |> List.flatten()
+        |> Enum.map(&String.to_integer/1)
+      end)
+
+    Enum.zip(times, distances)
+    |> Enum.map(fn {t, d} ->
+      0..t |> Enum.map(&((t - &1) * &1)) |> Enum.filter(&(&1 > d)) |> length()
+    end)
+    |> Enum.reduce(&Kernel.*/2)
+  end
+
+  def part2(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> length()
+  end
+end
+
+input = File.read!("./input/06.txt")
+
+IO.puts(Day06.part1(input))
+IO.puts(Day06.part2(input))
