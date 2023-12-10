1b96e0d4ab1b07a3f20ea37cfb3697c5a39fa9a4
Kirill Nikitin <locke23rus@gmail.com>
2023-12-10 22:53:46 +0200

    day 9 part 1

---
+++ 2023/day09.ex
@@ -0,0 +1,30 @@
+defmodule Day09 do
+  def part1(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      line
+      |> String.split(" ", trim: true)
+      |> Enum.map(&String.to_integer/1)
+      |> Stream.iterate(fn prev_list ->
+        Enum.chunk_every(prev_list, 2, 1, :discard)
+        |> Enum.map(fn [a, b] -> b - a end)
+      end)
+      |> Enum.take_while(fn list -> Enum.any?(list, &(&1 != 0)) end)
+      |> Enum.map(&List.last/1)
+      |> Enum.sum()
+    end)
+    |> Enum.sum()
+  end
+
+  def part2(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> length()
+  end
+end
+
+input = File.read!("./input/09.txt")
+
+IO.puts(Day09.part1(input))
+IO.puts(Day09.part2(input))
